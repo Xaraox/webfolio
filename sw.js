b390c0dcde73c7c48b568b7538d47b1135b06f04
Arvy <arvy.au@gmail.com>
2021-08-23 14:08:16 +1000

    Updates

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-407502a8f957056a1ce0.js"
+    "url": "webpack-runtime-ccce90588d38e3db4ec6.js"
   },
   {
     "url": "framework-2d6b7d2e2be378715b78.js"
@@ -37,7 +37,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "ccb14c8c8ce79698b7d10bc46bb6bc60"
+    "revision": "b245d94b07f9392ed2c6c32cd879a615"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-780d0321bb4481125aff.js"
@@ -48,7 +48,7 @@ self.__precacheManifest = [
   },
   {
     "url": "page-data/app-data.json",
-    "revision": "94ce045560ac70fc9003e66fef7d2252"
+    "revision": "e3013a9dfe3b4f5ad32023e1e1bceba9"
   },
   {
     "url": "polyfill-57963805237940a21f92.js"
